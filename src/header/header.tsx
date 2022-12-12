af4c79d9ccfd6228f41e7b6d92945627c60f654b
Eprst-p <easter_p@mail.ru>
2022-12-12 15:47:42 +0600

    some initiation

---
+++ src/header/header.tsx
@@ -0,0 +1,12 @@
+import './header.less';
+
+function Header(): JSX.Element {
+
+    return (
+        <header className="header">
+            <h1>Список запланированных дел</h1>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/main/tasks-list.tsx
@@ -0,0 +1,11 @@
+
+function TasksList(): JSX.Element {
+
+    return (
+        <div className="tasks-list">
+            Делишки дела
+        </div>
+    );
+}
+
+export default TasksList;
